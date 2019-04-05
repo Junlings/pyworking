fbc83f8a8e1aa9a468d0c26a231ab4945694acbf
junlings <xiajun19800812@hotmail.com>
2019-04-05 19:29:33 +0800

    try

---
+++ aa.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+if __name__ == '__main__':
+    a = np.array([1, 2, 3])
+    print 'aaa'
+
+    N = 50
+    x = np.random.rand(N)
+    y = np.random.rand(N)
+    plt.scatter(x,y)
+    plt.show()
+
+    print 1
